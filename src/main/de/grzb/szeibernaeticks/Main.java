6cf957a6039b1bfaefd9eddac6ab4952932f3a05
yuri <yuri@kanbaru.fritz.box>
2017-04-01 00:51:16 +0200

    useless commit

--- src/main/de/grzb/szeibernaeticks/Main.java
+++ src/main/de/grzb/szeibernaeticks/Main.java
@@ -9,7 +9,7 @@ import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 
 @Mod(modid = Main.MODID, version = Main.VERSION)
 public class Main
-{
+{ //TODO: move curly brace up
     public static final String MODID = "Szeibernaetics";
     public static final String VERSION = "0.1";
     
